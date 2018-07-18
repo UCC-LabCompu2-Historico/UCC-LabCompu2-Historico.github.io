af6df6f7a301c2aa1df19976d05547d0589ae1de
Ricardo Martin Marcucci <mmarcucci@ucc.edu.ar>
2018-07-18 13:22:28 -0300

    * Se cambio Angular por Vue.js * Se cambio autor1 y autor2 por array de autores * Se separaron los datos a otro archivo * Se cambió de tabla a Cards de Bootstrap * Se cambió la URL por nombre del repo

---
+++ data.js
@@ -0,0 +1,128 @@
+var datos = [
+    {
+        "tema": "Trayectoria de Tiro Oblicuo",
+        "url": "proyecto2016-trayectoriaTiroOblicuo",
+        "autores": ["Lautaro Garcia Celador"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Tiro Oblicuo",
+        "url": "proyecto2016-tiroOblicuo/principal.html",
+        "autores": ["Mirco Fiocchi","Franco Truffa"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Energia",
+        "url": "proyecto2016-Energia",
+        "autores": ["Franco Ferreyra","Damian De la Fuente"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Plano Inclinado",
+        "url": "proyecto2016-planoInclinado",
+        "autores": ["Enzo Dall Colle","Nicolas Gangi"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Conservación de Energía",
+        "url": "FisicaConservacionEnergia",
+        "autores": ["Sebastian Murgui","Ian Horrocks"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Tiro Parabólico",
+        "url": "proyecto2016-proyectil",
+        "autores": ["Nicolas Kolbelt"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Tiro Parabólico",
+        "url": "proyecto2017-tiroParabolico",
+        "autores": ["Azul Andrade","Laura Pognante"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Encuentro",
+        "url": "proyecto2016-encuentro",
+        "autores": ["Julieta Bertorello","Rocio Piantoni"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Péndulo Balistico",
+        "url": "proyecto2016-penduloBalistico/Index.html",
+        "autores": ["Juan Ignacio Catalano","Juan Cataneo"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Palanca",
+        "url": "proyecto2016-palanca",
+        "autores": ["Federico Montalvo","Federico Baiocco"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Óptica: Refracción",
+        "url": "Proyecto-Laboratorio/Contenido.html",
+        "autores": ["Ignacio Achaval"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Ley de Snell",
+        "url": "proyecto2017-LeySnell",
+        "autores": ["Sofía Cortez","Nicolas Kochlowski"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Péndulo Cónico",
+        "url": "proyecti2017-penduloConico/CodigoPenduloConico.html",
+        "autores": ["Nicolás Grupe","Marcos Lozada"],
+        "anio": "2016"
+    },
+    {
+        "tema": "Presion Hidroestática",
+        "url": "proyecto-duckyweb",
+        "autores": ["Nelo Gino Nanfara","Roberto Leandro Ruiz"],
+        "anio": "2017"
+    },
+    {
+        "tema": "Cargas Eléctricas",
+        "url": "proyecto-coulombfordummies",
+        "autores": ["Santiago Diaz Gonzalez","Facundo Bayona"],
+        "anio": "2017"
+    },
+    {
+        "tema": "Sistema de Gestión",
+        "url": "proyecto-senestrari",
+        "autores": ["Francisco Senestrari"],
+        "anio": "2017"
+    },
+    {
+        "tema": "Circuitos RC",
+        "url": "proyecto2018-mirandalabii",
+        "autores": ["Matias Miranda"],
+        "anio": "2018"
+    },
+    {
+        "tema": "Rozamiento",
+        "url": "proyecto2018-luzuriaga-galmes-lab-compu-ii/index.html",
+        "autores": ["Facundo Luzuriaga","Francisco Galmes"],
+        "anio": "2018"
+    },
+    {
+        "tema": "Simulador del Sistema Solar",
+        "url": "proyecto-2018-Nievas-Bobadilla",
+        "autores": ["Franco Bobadilla","Agustín Nievas"],
+        "anio": "2018"
+    },
+    {
+        "tema": "Simulador del Sistema Solar",
+        "url": "proyecto2018-estrella",
+        "autores": ["Carolina Misa","Matías Insaurralde"],
+        "anio": "2018"
+    },
+    {
+        "tema": "Difracción de la Luz",
+        "url": "proyecto2018-rodriteam",
+        "autores": ["Rodrigo Pizarro"],
+        "anio": "2018"
+    }
+];
